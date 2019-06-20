5b612dcf069911ae7b5879eaafe71bf8ea319385
pkellner <peter@peterkellner.net>
2019-06-20 15:42:08 -0700

    in progress...

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 
-  
+
 
 
 
@@ -9,13 +9,24 @@ window.onload = function() {
   timeNow.innerText = new Date().toLocaleTimeString();
   var resultsButton = document.getElementById("buttonId");
   resultsButton.onclick = () => {
-    chrome.runtime.sendMessage(
-      "FromPopupToBackground:" + timeNow.innerText,
-      function(response) {
-        response === undefined
-          ? alert("popup.js:return from sendMessage:undefined")
-          : alert("popup.js:return from sendMessage:", response);
-      }
-    );
+
+    chrome.storage.sync.get(["testSyncValue"], result => {
+      request = `popup.js:testSyncValue:${result.testSyncValue}`;
+      chrome.tabs.sendMessage(activeTabId, { data: request }, function(response) {
+        sendResponse(
+          `background.js sending back to popup because done now.  synchronous response from message sent to context.js:${response}`
+        );
+      });
+    });
+
+
+    // chrome.runtime.sendMessage(
+    //   "FromPopupToBackground:" + timeNow.innerText,
+    //   function(response) {
+    //     response === undefined
+    //       ? alert("popup.js:return from sendMessage:undefined")
+    //       : alert("popup.js:return from sendMessage:", response);
+    //   }
+    // );
   };
 };
